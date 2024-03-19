6b0897f7939769de5d8e1dacf7c1374e88ea2484
Pia <gayeongparkk@gmail.com>
2024-03-19 10:55:09 +0900

    cleanup & fix chain id err

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,71 @@
 mod rpc;
 mod tx;
 
-use rpc::RpcProvider;
-use alloy_primitives::{U256, B256};
-use alloy_rlp;
-use alloy_provider::{Provider};
-use alloy_consensus::SignableTransaction;
-use alloy_rlp::Encodable;
+use crate::tx::{ConsensusTx, RpcTx};
 use alloy_network::eip2718::Encodable2718;
-use std::sync::Arc;
+use alloy_primitives::{B256, U256, U64};
+use clap::Parser;
 use eth_trie::MemoryDB;
 use eth_trie::{EthTrie, Trie, TrieError};
-use crate::tx::{ConsensusTx, RpcTx};
+use rpc::RpcProvider;
+use std::str::FromStr;
+use std::sync::Arc;
+
+/// Simple Herodotus Data Processor CLI to handle tasks and datalakes
+#[derive(Debug, Parser)]
+#[command(name = "hdp")]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    tx_hash: String,
+
+    #[arg(default_value_t = 1, short, long)]
+    chain_id: u64,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let args = Cli::parse();
+    let tx_hash = B256::from_str(&args.tx_hash).map_err(|_| Error::ConversionError)?;
+    let chain_id = U64::from(args.chain_id);
     let provider = RpcProvider::new();
-    let height = RpcProvider::get_tx_block_height(&provider, B256::from([38,86,13,104,96,219,186,48,160,248,209,237,235,63,142,200,50,170,203,16,24,3,139,33,33,179,162,46,93,52,59,44])).await?;
+    let height = RpcProvider::get_tx_block_height(&provider, tx_hash).await?;
     println!("Height: {:?}", height);
-    let txs = provider.get_block_transactions(height).await?;
-    println!("Tx count: {:?}", txs.len());
-     // Refactored line: Properly handling errors within the map operation before collecting.
-    let converted: Vec<ConsensusTx> = txs.iter()
-        .map(|tx| RpcTx(tx.clone()).try_into())
-        .collect::<Result<Vec<_>, _>>().map_err(|_| Error::ConversionError)?;
+    let mut txs_with_root = provider.get_block_transactions(height).await?;
+    println!("Tx count: {:?}", txs_with_root.transactions.len());
+    println!("Root from header: {:?}", txs_with_root.root);
+    // Refactored line: Properly handling errors within the map operation before collecting.
+    let converted: Vec<ConsensusTx> = txs_with_root
+        .transactions
+        .iter_mut()
+        .map(|tx| {
+            if tx.chain_id.is_none() {
+                tx.chain_id = Some(chain_id);
+            }
+            RpcTx(tx.clone()).try_into()
+        })
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|_| Error::ConversionError)?;
 
     let memdb = Arc::new(MemoryDB::new(true));
     let mut trie = EthTrie::new(memdb.clone());
 
     converted.iter().enumerate().for_each(|(idx, tx)| {
-        let key = alloy_rlp::encode(&U256::from(idx));
+        let key = alloy_rlp::encode(U256::from(idx));
         let rlp = tx.0.encoded_2718();
         trie.insert(key.as_slice(), rlp.as_slice()).unwrap();
     });
 
-    let new_root = trie.root_hash();
+    let new_root = trie.root_hash().unwrap();
     println!("NeRoot: {:?}", new_root);
+
+    if txs_with_root.root != new_root.to_fixed_bytes() {
+        // TODO: If eip4844 tx exist, currently root is not matching
+        println!("Root mismatch");
+    }
+
     Ok(())
-
 }
-
 
 #[derive(Debug)]
 pub enum Error {
@@ -48,5 +74,5 @@ pub enum Error {
     RPC(String),
     TxNotFound,
     InvalidTxVersion,
-    ConversionError
+    ConversionError,
 }
--- src/rpc.rs
+++ src/rpc.rs
@@ -1,50 +1,64 @@
+use crate::Error;
 use alloy_network::Ethereum;
-use alloy_primitives::{B256, U256};
+use alloy_primitives::B256;
 use alloy_provider::{Provider, ProviderBuilder, RootProvider};
 use alloy_rpc_client::RpcClient;
 use alloy_rpc_types::{BlockTransactions, Transaction};
 use alloy_transport_http::Http;
 use reqwest::Client;
-use crate::Error;
 
 pub struct RpcProvider {
     provider: RootProvider<Ethereum, Http<Client>>,
 }
 
+pub struct TransactionsWithRoot {
+    pub transactions: Vec<Transaction>,
+    pub root: B256,
+}
+
 impl RpcProvider {
     pub fn new() -> Self {
-        let http = Http::<Client>::new("https://mainnet.infura.io/v3/da91aac0e91048b3bf3be813262d43a6".to_string().parse().unwrap());
+        let http = Http::<Client>::new(
+            "https://mainnet.infura.io/v3/da91aac0e91048b3bf3be813262d43a6"
+                .to_string()
+                .parse()
+                .unwrap(),
+        );
         let provider = ProviderBuilder::<_, Ethereum>::new()
             .provider(RootProvider::new(RpcClient::new(http, true)));
-        Self {
-            provider
-        }
+        Self { provider }
     }
 
-    pub async fn get_block_transactions(&self, block_number: u64) -> Result<Vec<Transaction>, Error> {
-        let block = self.provider
+    pub async fn get_block_transactions(
+        &self,
+        block_number: u64,
+    ) -> Result<TransactionsWithRoot, Error> {
+        let block = self
+            .provider
             .get_block(block_number.into(), true)
             .await
             .map_err(|err| Error::RPC(err.to_string()))?
             .ok_or_else(|| Error::RPC("Unable to retrieve block".to_string()))?;
 
-        println!("TxRoot: {:?}", block.header.transactions_root);
-
         match block.transactions {
-            BlockTransactions::Full(txs) => Ok(txs),
-            _ => return Err(Error::RPC("Unable to retrieve block txs".to_string()))
+            BlockTransactions::Full(txs) => Ok(TransactionsWithRoot {
+                transactions: txs,
+                root: block.header.transactions_root,
+            }),
+            _ => Err(Error::RPC("Unable to retrieve block txs".to_string())),
         }
     }
 
     pub async fn get_tx_block_height(&self, tx_hash: B256) -> Result<u64, Error> {
-        let tx = self.provider
-            .get_transaction_by_hash(tx_hash.into())
+        let tx = self
+            .provider
+            .get_transaction_by_hash(tx_hash)
             .await
             .map_err(|err| Error::RPC(err.to_string()))?;
 
         let height: u64 = match tx.block_number {
             Some(height) => height.try_into().map_err(|_| Error::TxNotFound)?,
-            None => return Err(Error::TxNotFound)
+            None => return Err(Error::TxNotFound),
         };
 
         Ok(height)
