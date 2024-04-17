3dcc33cec133fd9c6b5501fb66555c13602d57ba
Pia <gayeongparkk@gmail.com>
2024-04-17 14:02:17 +0900

    feat: consensus tx receipt abstracted methods

--- src/tx_receipt.rs
+++ src/tx_receipt.rs
@@ -1,13 +1,61 @@
 use crate::Error;
 use alloy_consensus::{Receipt, ReceiptWithBloom};
 use alloy_consensus::{ReceiptEnvelope, TxType};
-
+use alloy_eips::eip2718::Decodable2718;
+use alloy_network::eip2718::Encodable2718;
 use alloy_primitives::U64;
 use alloy_primitives::{Bloom, Log, LogData, U8};
 use alloy_rpc_types::{Log as RpcLog, TransactionReceipt};
 
 #[derive(Debug, Clone)]
 pub struct ConsensusTxReceipt(pub ReceiptEnvelope);
+
+impl ConsensusTxReceipt {
+    pub fn rlp_encode(&self) -> Vec<u8> {
+        self.0.encoded_2718()
+    }
+
+    pub fn rlp_decode(mut data: &[u8]) -> Result<Self, Error> {
+        let envelope = ReceiptEnvelope::decode_2718(&mut data).map_err(Error::Rlp)?;
+        Ok(ConsensusTxReceipt(envelope))
+    }
+
+    pub fn success(&self) -> bool {
+        match &self.0 {
+            ReceiptEnvelope::Legacy(receipt) => receipt.receipt.success,
+            ReceiptEnvelope::Eip2930(receipt) => receipt.receipt.success,
+            ReceiptEnvelope::Eip1559(receipt) => receipt.receipt.success,
+            ReceiptEnvelope::Eip4844(receipt) => receipt.receipt.success,
+        }
+    }
+
+    pub fn cumulative_gas_used(&self) -> u64 {
+        match &self.0 {
+            ReceiptEnvelope::Legacy(receipt) => receipt.receipt.cumulative_gas_used,
+            ReceiptEnvelope::Eip2930(receipt) => receipt.receipt.cumulative_gas_used,
+            ReceiptEnvelope::Eip1559(receipt) => receipt.receipt.cumulative_gas_used,
+            ReceiptEnvelope::Eip4844(receipt) => receipt.receipt.cumulative_gas_used,
+        }
+    }
+
+    pub fn logs(&self) -> Vec<Log<LogData>> {
+        match &self.0 {
+            ReceiptEnvelope::Legacy(receipt) => receipt.receipt.logs.clone(),
+            ReceiptEnvelope::Eip2930(receipt) => receipt.receipt.logs.clone(),
+            ReceiptEnvelope::Eip1559(receipt) => receipt.receipt.logs.clone(),
+            ReceiptEnvelope::Eip4844(receipt) => receipt.receipt.logs.clone(),
+        }
+    }
+
+    pub fn bloom(&self) -> Bloom {
+        match &self.0 {
+            ReceiptEnvelope::Legacy(receipt) => receipt.bloom,
+            ReceiptEnvelope::Eip2930(receipt) => receipt.bloom,
+            ReceiptEnvelope::Eip1559(receipt) => receipt.bloom,
+            ReceiptEnvelope::Eip4844(receipt) => receipt.bloom,
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub(crate) struct RpcTxReceipt(pub TransactionReceipt);
